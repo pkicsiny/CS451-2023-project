55abe054c2589e68c0e3d5bc9a6bb1147a6b1747
Peter Kicsiny <pkicsiny@gmail.com>
2023-10-17 10:50:54 +0200

    add ack system

--- template_cpp/src/src/main.cpp
+++ template_cpp/src/src/main.cpp
@@ -144,6 +144,36 @@ int main(int argc, char **argv) {
   int serv_port = hosts[config_i-1].port;
   std::cout << "server IP:" << serv_ip << ", server port:" << serv_port << std::endl;
 
+  /*-------------------*/
+  // init message list //
+  /*-------------------*/
+
+  std::vector<int> msg_list(config_m);
+  for (int i=0; i<config_m; i++){
+    msg_list[i] = i+1;
+  }
+  std::cout << "message list:" << std::endl;
+  for (auto const& msg_i : msg_list){
+    std::cout << msg_i << std::endl;
+  }
+
+  /*-------------------------*/
+  // init pid msg count dict //
+  /*-------------------------*/
+  
+  std::map<int64_t, std::map<int64_t, int64_t> > pid_msg_count_dict;
+  for (auto &host : hosts) {
+    for (auto const& msg_i : msg_list){
+      pid_msg_count_dict[host.port][msg_i] = 0;
+    }
+  }
+
+  // recv timeout
+  struct timeval read_timeout;
+  read_timeout.tv_sec = 1;
+  read_timeout.tv_usec = 0;
+
+
   /*---------------*/
   // create socket //
   /*---------------*/
@@ -198,6 +228,8 @@ int main(int argc, char **argv) {
     // wait for messages incoming to server's port
     std::cout << "Waiting to receive messages..." << std::endl;
     while(true){
+
+      // blocking recv
       int64_t msg_recv = recvfrom(socket_fd, msg_buf, sizeof(msg_buf), 0,
           reinterpret_cast<struct sockaddr *>(&client_addr), &client_addr_size);  // returns length of incoming message
       if (msg_recv < 0) {
@@ -208,28 +240,45 @@ int main(int argc, char **argv) {
         int client_port = ntohs(client_addr.sin_port); 
         std::cout << "Successfully received message: " << msg_buf << ", having length " << msg_recv << ", from port: " << client_port << std::endl;
 
-        // upon successful receive trigger event send ack
-        std::string msg_ack = "ack";
-        std::cout << "Sending message: " << msg_ack << ", of length: " << msg_ack.length() << ", from port: " << my_port << ", to port: " << client_port << std::endl;
+        // upon successful receive trigger event send ack once, ack is "port msg"
+        std::string msg_ack = std::to_string(client_port) + " " + msg_buf;
+        std::cout << "Sending ack message: " << msg_ack << ", of length: " << msg_ack.length() << ", from port: " << my_port << ", to port: " << client_port << std::endl;
         int64_t msg_ack_send = sendto(socket_fd, msg_ack.c_str(), msg_ack.length(), 0,
             reinterpret_cast<struct sockaddr *>(&client_addr), sizeof(client_addr));  // returns number of characters sent
         if (msg_ack_send < 0) {
-            std::cout << "Sending message " << msg_ack << " failed with error" << std::endl;
+            std::cout << "Sending ack message " << msg_ack << " failed with error" << std::endl;
             return -1;
         }else{
-            std::cout << "Send successful: " << msg_ack_send << std::endl;
+            std::cout << "[Send ack successful]" << std::endl;
         }
 
         // if message is duplicate, do not store in log (if not in dict add to it)
         int64_t client_pid = port_proc_dict[client_port];
+
+        // pid is not in dict i.e. this is the first msg from proc pid
         if (proc_recv_dict.find(client_pid) == proc_recv_dict.end()) {
-          // pid is not in dict i.e. this is the first msg from proc pid
           proc_recv_dict[client_pid].push_back(msg_buf);
+
+          // log into
+          std::ofstream output_file;
+          output_file.open(parser.outputPath(), std::ios_base::app);
+          if (output_file.is_open()){
+            output_file << "d " << client_pid << " " << msg_buf << std::endl;
+            output_file.close();
+          }else{
+            std::cout << "Could not open output file: " << parser.outputPath() << std::endl;
+            return -1;
+          }
+
+        // pid is already in dict i.e. msg might be a duplicate
         } else {
-          // pid is already in dict i.e. msg might be a duplicate
+
           // if this is true msg_buf is not yet in dict[pid]
           if (std::find(proc_recv_dict[client_pid].begin(), proc_recv_dict[client_pid].end(), msg_buf) == proc_recv_dict[client_pid].end()){
             // msg is not yet in dict so log it
+            proc_recv_dict[client_pid].push_back(msg_buf);
+
+            // log into output
             std::ofstream output_file;
             output_file.open(parser.outputPath(), std::ios_base::app);
             if (output_file.is_open()){
@@ -238,12 +287,23 @@ int main(int argc, char **argv) {
             }else{
               std::cout << "Could not open output file: " << parser.outputPath() << std::endl;
               return -1;
-            } // end if
+            }
+
           } // end if
         } // end if
 
-      } // if
-    } // while
+        // print received messages
+        std::cout << "Received messages:" << std::endl;
+        for (auto const &pair: proc_recv_dict) {
+          std::cout << "PID " << pair.first << ": [";
+          for (auto const& i: pair.second){
+            std::cout << i << ", ";
+          }
+          std::cout << "]" << std::endl;
+        }
+
+      } // if (msg_recv < 0)
+    } // while recv
  
   /*--------*/
   // client //
@@ -275,18 +335,31 @@ int main(int argc, char **argv) {
       return -1;     
     }
 
-    std::cout << "Starting sending messages..." << std::endl;
-    for (int msg_i=1; msg_i<=config_m; msg_i++){
-      // send each message in a while loop that terminates only when client receives the ack from server
-      std::string msg_i_str = std::to_string(msg_i);
-      std::cout << "Sending message: " << msg_i_str << ", of length: " << msg_i_str.length() << ", from port: " << my_port << ", to port: " << serv_port << std::endl;
-      int64_t msg_i_send_count = 0;
-      while(true){ 
+    // set timeout on socket
+    setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof read_timeout);
+
+    char ack_buf[128];  // buffer for messages
+    socklen_t serv_addr_size  = sizeof(serv_addr);
+
+
+    std::cout << "Start sending messages..." << std::endl;
+    while(!msg_list.empty()){ 
+      std::cout << "Messages in message list to send: [";
+      for (auto const& i : msg_list){
+        std::cout << i << ", ";
+      }
+      std::cout << "]" << std::endl;
+
+      for (auto const& msg_i : msg_list){  // loop over messages
+        // send each message in a while loop that terminates only when client receives the ack from server
+        std::string msg_i_str = std::to_string(msg_i);
+        std::cout << "Sending message: " << msg_i_str << ", of length: " << msg_i_str.length() << ", from port: " << my_port << ", to port: " << serv_port << std::endl;
+
         int64_t msg_send = sendto(socket_fd, msg_i_str.c_str(), msg_i_str.length(), 0,
             reinterpret_cast<struct sockaddr *>(&serv_addr), sizeof(serv_addr));  // returns number of characters sent
         
         // sender logs broadcast event only once at first send
-        if (msg_i_send_count == 0){
+        if (pid_msg_count_dict[serv_port][msg_i] == 0){
           std::ofstream output_file;
           output_file.open(parser.outputPath(), std::ios_base::app);
           if (output_file.is_open()){
@@ -297,27 +370,68 @@ int main(int argc, char **argv) {
             return -1;
           } // end if
         } // end if
-        msg_i_send_count += 1;
- 
+        
+        // increment sent msg_i to pid once more
+        pid_msg_count_dict[serv_port][msg_i] += 1;
+
         if (msg_send < 0) {
-            std::cout << "Send failed with error" << std::endl;
+            std::cout << "[Send ERROR]" << std::endl;
             return -1;
         }else{
-            std::cout << "Send successful: " << msg_send << std::endl;
-
-            // TODO: exit while upon reception of ack
-
+            std::cout << "[Send successful]" << std::endl;
         } // end if
-      } // end while
-    } // end for
-  } // end if
+      } // end for
+
+      // listen to acks, ack is a tuple of (pdi, msg)
+      std::cout << "Listening to acks..." << std::endl;
+      while(true){
+
+        // this is blocking so it listens indefinitely; make it nonblocking by setting a timeout
+        int64_t ack_recv = recvfrom(socket_fd, ack_buf, sizeof(ack_buf), 0,
+            reinterpret_cast<struct sockaddr *>(&serv_addr), &serv_addr_size);  // returns length of incoming message
+        if (ack_recv < 0) {
+          break; // terminate while loop i.e. no more ack to receive
+        }else if (ack_recv != 0) {
+          ack_buf[ack_recv] = '\0'; //end of line to truncate junk
+          int serv_port = ntohs(serv_addr.sin_port); 
+          std::cout << "Successfully received ack message: " << ack_buf << ", having length " << ack_recv << ", from port: " << serv_port << std::endl;
+          // split string
+          std::stringstream ss(ack_buf);
+          std::string word;
+          std::vector<int> ack_vec;
+    
+          while (std::getline(ss, word, ' ')) {
+            ack_vec.push_back(std::stoi(word));
+          }
+  
+          // get index of msg in msg_list
+          uint64_t msg_list_idx = std::distance(msg_list.begin(), std::find(msg_list.begin(), msg_list.end(), ack_vec[1]));
+
+          if(msg_list_idx >= msg_list.size()) {
+            std::cout << "Ack msg " << ack_vec[1] << " not in msg_list" << std::endl;
+          } else {
+            // remove acked msg from msg_list
+            msg_list.erase(msg_list.begin()+msg_list_idx);
+            std::cout << "msg_list after removing acked " << ack_vec[1] << ": [";
+            for (auto const& i : msg_list){
+              std::cout << i << ", ";
+            }
+            std::cout << "]" << std::endl;
+          }  // end if
+        }  // end if (ack_recv < 0)
+      }  // end while recv
+
+  }  // end while send
+
+  std::cout << "Finished broadcasting." << std::endl;
+}  // end if
 
   // After a process finishes broadcasting,
   // it waits forever for the delivery of messages.
-  while (true) {
+//  while (true) {
     
     //std::this_thread::sleep_for(std::chrono::hours(1));
-  }
+//  }
 
   return 0;
 }
